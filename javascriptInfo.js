262aea5aa681129207e286e21405fbdd9f589b30
Jiapan_Yu <2541771811@qq.com>
2020-07-01 13:42:01 +0800

    Objects

--- javascriptInfo.js
+++ javascriptInfo.js
@@ -1017,3 +1017,63 @@ if (n <= 0) {
 } else {
   alert( pow(x, n) );
 } */
+
+
+
+// Objects
+/* let user = {}
+user.name = 'John'
+user.surname = 'Smith'
+user.name = 'Pete'
+delete user.name */
+
+
+/* function isEmpty(obj) {
+  for (let v in obj) {
+    // if the loop has started, there's a property
+    return false
+  }
+
+  return true
+}
+
+let schedule = {};
+
+alert( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+
+alert( isEmpty(schedule) ); // false */
+
+
+/* let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+}
+
+let sum = 0
+for (let salary in salaries) {
+  sum += salaries[salary]
+}
+
+alert(sum) */
+
+
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+}
+
+multiplyNumeric(menu)
+
+function multiplyNumeric(obj) {
+  for (let key in obj) {
+    if (typeof obj[key] === 'number') {
+      obj[key] *= 2
+    }
+  }
+}
+
+console.log(menu)
