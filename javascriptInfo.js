a81a69e4573e0e8d4b584dc421d3863d960880bf
Jiapan_Yu <2541771811@qq.com>
2021-05-05 16:58:08 +0800

    Custom errors, extending Error

--- javascriptInfo.js
+++ javascriptInfo.js
@@ -3595,7 +3595,7 @@ readData(); */
   }
 } */
 
-function isFinal() {
+/* function isFinal() {
   try {
     console.log("work");
     return true;
@@ -3606,7 +3606,31 @@ function isFinal() {
   console.log("cleanup the working space");
 }
 
-console.log(isFinal());
+console.log(isFinal()); */
+
+
+
+// Custom errors, extending Error
+/* class ValidationError {}
+
+Object.assign(ValidationError.prototype, Error); */
+
+class FormatError extends SyntaxError {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
+
+let err = new FormatError("formatting error");
+
+console.log( err.message ); // formatting error
+console.log( err.name ); // FormatError
+console.log( err.stack ); // stack
+
+console.log( err instanceof FormatError ); // true
+console.log( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
+
 
 
 
