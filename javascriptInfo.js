986b5cb14993752b063fa6fcaf4cfb653bd1af48
Jiapan_Yu <2541771811@qq.com>
2021-05-07 11:32:42 +0800

    Promise

--- javascriptInfo.js
+++ javascriptInfo.js
@@ -3615,7 +3615,7 @@ console.log(isFinal()); */
 
 Object.assign(ValidationError.prototype, Error); */
 
-class FormatError extends SyntaxError {
+/* class FormatError extends SyntaxError {
   constructor(message) {
     super(message);
     this.name = this.constructor.name;
@@ -3629,7 +3629,77 @@ console.log( err.name ); // FormatError
 console.log( err.stack ); // stack
 
 console.log( err instanceof FormatError ); // true
-console.log( err instanceof SyntaxError ); // true (because inherits from SyntaxError)
+console.log( err instanceof SyntaxError ); // true (because inherits from SyntaxError) */
+
+
+
+// Promise
+// new Promise((resolve, reject) => {})
+
+/* let promise = new Promise(function(resolve, reject) {
+  // the function is executed automatically when the promise is constructed
+
+  // after 1 second signal that the job is done with the result "done"
+  setTimeout(() => {
+    console.log(promise);
+    resolve("done")
+  }, 1000);
+});
+
+console.log(promise); */
+
+/* let promise = new Promise((resolve, reject) => {
+  setTimeout(() => reject(new Error("Whoops!")), 1000);
+});
+
+promise.catch(console.log); */
+
+/* let promise = new Promise(function(resolve, reject) {
+  resolve(1);
+
+  setTimeout(() => resolve(2), 1000);
+});
+
+promise.then(alert); */
+
+/* function delay(ms) {
+  // your code
+  return new Promise((resolve, reject)=> {
+    setTimeout(resolve, ms);
+  })
+}
+
+delay(3000).then(() => alert('runs after 3 seconds')); */
+
+/* function go() {
+  showCircle(150, 150, 100).then(div => {
+    console.log(div);
+    div.classList.add('message-ball');
+    div.append("Hello, world!");
+  });
+}
+
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0);
+  });
+} */
 
 
 
