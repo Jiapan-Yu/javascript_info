322e3e1962d530024c336017e37e56b26933e6c4
Jiapan_Yu <2541771811@qq.com>
2021-05-11 17:26:13 +0800

    Async/await

--- javascriptInfo.js
+++ javascriptInfo.js
@@ -3814,11 +3814,95 @@ Promise.allSettled(urls.map(url => fetch(url)))
 
 
  // Microtasks
- let promise = Promise.reject(new Error("Promise Failed!"));
+/*  let promise = Promise.reject(new Error("Promise Failed!"));
 setTimeout(() => promise.catch(err => alert('caught')), 1000);
 
 // Error: Promise Failed!
-window.addEventListener('unhandledrejection', event => alert(event.reason));
+window.addEventListener('unhandledrejection', event => alert(event.reason)); */
+
+
+
+// Async/await
+/* async function showAvatar() {
+
+  // read our JSON
+  let response = await fetch('https://javascript.info/article/promise-chaining/user.json');
+  let user = await response.json();
+
+  // read github user
+  let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+  let githubUser = await githubResponse.json();
+
+  // show the avatar
+  let img = document.createElement('img');
+  img.src = githubUser.avatar_url;
+  img.className = "promise-avatar-example";
+  document.body.append(img);
+
+  // wait 3 seconds
+  await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+
+  img.remove();
+
+  return githubUser;
+}
+
+console.log(showAvatar()); */
+
+
+//tasks
+/* async function loadJson(url) {
+  let response = await fetch(url);
+  
+  if (response.status == 200) {
+    return response.json();
+  } else {
+    throw new Error(response.status);
+  }
+}
+
+loadJson('no-such-user.json').catch(alert); */
+
+class HttpError extends Error {
+  constructor(response) {
+    super(`${response.status} for ${response.url}`);
+    this.name = 'HttpError';
+    this.response = response;
+  }
+}
+
+function loadJson(url) {
+  return fetch(url)
+    .then(response => {
+      if (response.status == 200) {
+        return response.json();
+      } else {
+        throw new HttpError(response);
+      }
+    });
+}
+
+// Ask for a user name until github returns a valid user
+function demoGithubUser() {
+  let name = prompt("Enter a name?", "iliakan");
+
+  return loadJson(`https://api.github.com/users/${name}`)
+    .then(user => {
+      console.log("user: ", user);
+      alert(`Full name: ${user.name}.`);
+      return user;
+    })
+    .catch(err => {
+      if (err instanceof HttpError && err.response.status == 404) {
+        alert("No such user, please reenter.");
+        return demoGithubUser();
+      } else {
+        throw err;
+      }
+    });
+}
+
+demoGithubUser();
 
 
 
