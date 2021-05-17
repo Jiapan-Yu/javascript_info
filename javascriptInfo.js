4e7e1d0e00fb471707c9e3a154f1b41e0c68e4d1
Jiapan_Yu <2541771811@qq.com>
2021-05-17 12:31:00 +0800

    Async iteration and generators

--- javascriptInfo.js
+++ javascriptInfo.js
@@ -3377,6 +3377,28 @@ alert(rabbit.name); */
 
 
 
+// Class inheritance
+/* class Animal {
+  constructor(name) {
+    this.speed = 0;
+    this.name = name;
+  }
+  run(speed) {
+    this.speed = speed;
+    alert(`${this.name} runs with speed ${this.speed}.`);
+  }
+  stop() {
+    this.speed = 0;
+    alert(`${this.name} stands still.`);
+  }
+}
+
+let animal = new Animal("My animal");
+
+console.log(animal.__proto__); */
+
+
+
 // Static properties and methods
 /* class Animal {
   static planet = "Earth";
@@ -4093,6 +4115,103 @@ let generator = pseudoRandom(1);
 console.log(generator.next().value); // 16807
 console.log(generator.next().value); // 282475249
 console.log(generator.next().value); // 1622650073 */
+
+
+
+// Async iteration and generators
+/* let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.iterator]() { // called once, in the beginning of for..of
+    return {
+      current: this.from,
+      last: this.to,
+
+      next() { // called every iteration, to get the next value
+        console.log("this: ", this);
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+};
+
+for(let value of range) {
+  alert(value); // 1 then 2, then 3, then 4, then 5
+} */
+
+
+/* let range = {
+  from: 1,
+  to: 5,
+
+  [Symbol.asyncIterator]() { // called once, in the beginning of for..of
+    return {
+      current: this.from,
+      last: this.to,
+
+      async next() { // called every iteration, to get the next value
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+        if (this.current <= this.last) {
+          return { done: false, value: this.current++ };
+        } else {
+          return { done: true };
+        }
+      }
+    };
+  }
+};
+
+(async () => {
+  for await (let value of range) {
+    console.log(value); // 1 then 2, then 3, then 4, then 5
+  }
+})(); */
+
+
+async function* fetchCommits(repo) {
+  let url = `https://api.github.com/repos/${repo}/commits`;
+
+  while (url) {
+    const response = await fetch(url, { // (1)
+      headers: {'User-Agent': 'Our script'}, // github needs any user-agent header
+    });
+
+    const body = await response.json(); // (2) response is JSON (array of commits)
+    console.log("body: ", body);
+
+    // (3) the URL of the next page is in the headers, extract it
+    console.log("response.headers: ", response.headers);
+    console.log("response.headers.get('Link'): ", response.headers.get('Link'));
+    // 怎么 define 一个 named capturing groups
+    let nextPage = response.headers.get('Link').match(/<(.*?)>; rel="next"/);
+    console.log("nextPage: ", nextPage);
+    nextPage = nextPage?.[1];
+
+    url = nextPage;
+
+    for(let commit of body) { // (4) yield commits one by one, until the page ends
+      yield commit;
+    }
+  }
+}
+
+(async () => {
+
+  let count = 0;
+
+  for await (const commit of fetchCommits('javascript-tutorial/en.javascript.info')) {
+    if (++count == 100) { // let's stop at 100 commits
+      break;
+    }
+  }
+
+})();
 
 
 
