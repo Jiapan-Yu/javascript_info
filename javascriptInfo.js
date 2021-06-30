210d14125b7f767fdf9ac31d22648fda084cd285
Jiapan_Yu <2541771811@qq.com>
2021-06-30 16:46:45 +0800

    Moving the mouse: mouseover/out, mouseenter/leave -- Event delegation finished

--- javascriptInfo.js
+++ javascriptInfo.js
@@ -11,97 +11,33 @@ let currentElem;
 table.onmouseover = function(event) { // relatedTarget ==> target
   let target = event.target;
   console.log("mouseover", target)
+  console.log("mouseover relatedTarget", event.relatedTarget)
 
-  currentElem = target.closest('td') || target; // 这一行很关键，如果直接从 td.w 到 td.sw 的 child strong，那么 target 就是 strong 了已理解
-
-
-  // 如果 mouseover target 不是 td 则退出
-  if (currentElem.tagName !== 'TD') return;
-
-  
-// 怎么和 fast movement 区分
-if (currentElem.tagName === event.relatedTarget.tagName) {
-  onEnter(currentElem)
-  return
-}
-  // 从 strong 进入到 td 和 从 td 进入到 child strong 
-  if (event.relatedTarget.closest('td')) return;
-
-
-  // 从 td 进入 child 退出
-  // 单独此行的话不行，因为从 table 进入 td 也会触发
-  // if (event.relatedTarget.contains(target)) return;
-
-  onEnter(currentElem)
-
-
-  
-  /*
-
-  if (currentElem) {
-    onLeave(currentElem)
+  if (currentElem && target.closest('td') === currentElem) {
+    return
   }
 
-  currentElem = target;
 
-  onEnter(target) */
+  if (target.closest('td')) {
+    currentElem = target.closest('td')
+    onEnter(target.closest('td'))
+  }
 };
 
 table.onmouseout = function(event) { // target ==> relatedTarget
   let target = event.target;
   console.log("mouseout target", target)
-  // console.log("mouseout relatedTarget", event.relatedTarget)
+  console.log("mouseout relatedTarget", event.relatedTarget)
 
-  // 如果 mouseout target 不是 td 则退出
-  if (target.tagName !== 'TD') return;
-
-
-// 怎么和 fast movement 区分
-if (target.tagName === event.relatedTarget.tagName) {
-  onLeave(target)
-  return
-}
-// 比如直接从 td.w 到 td.sw 的 child strong
-if (event.relatedTarget && event.relatedTarget.closest('td') !== currentElem) {
-  onLeave(target)
-  return
-}
-// 如果直接从 td.sw 的 child strong 到 table 呢？
-
-
-
-  // 从 strong 进入到 td 和 从 td 进入到 child strong 
-  if (event.relatedTarget && event.relatedTarget.closest('td')) return;
-  // 从 td 进入到 table
-  if (event.relatedTarget && event.relatedTarget.contains(currentElem)) {
-    onLeave(target)
+  if (event.relatedTarget && event.relatedTarget.closest('td') === currentElem) {
     return
   }
-  // 下面的一块代码跟上面两行解决同样的问题
-  /* let closestTd = currentElem.closest('td')
-  console.log("closestTd: ", closestTd)
-  // 仍然在 td 里面
-  // 进入到 child 时会触发
-  if (target === closestTd) return;
-  // 从 td child 进入 td
-  if (closestTd.tagName === 'TD') return; */
-
-  onLeave(target)
 
 
-
-  /* // mouseout relatedTarget null 情况
-  if (!event.relatedTarget) return;
-  
-  // 如果 relatedTarget 是 onmouseout td 则退出
-  if (event.relatedTarget.tagName === 'TD') return;
-
-  // 如果 target element 包含 relatedTarget element，则退出
-  if (target.contains(event.relatedTarget)) return;
-
-  if (target.tagName === 'TD') {
-    onLeave(target)
-  } */
+  if (target.closest('td')) {
+    currentElem = null;
+    onLeave(target.closest('td'))
+  }
 };
 
 function onEnter(target) {
