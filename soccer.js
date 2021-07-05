322ad3048c1fc2620899532b52c10a1dc9cf16e1
Jiapan_Yu <2541771811@qq.com>
2021-07-05 11:16:49 +0800

    'Drag superheroes around the field' target left top position

--- soccer.js
+++ soccer.js
@@ -1,30 +1,58 @@
 // Your code
-document.onmousedown = function(e) {
+document.onmousedown = function (e) {
   let target = e.target;
-  console.log("target: ", target)
-  let targetRect = target.getBoundingClientRect();
-  let shiftX = e.pageX - targetRect.left;
-  let shiftY = e.pageY - targetRect.top;
-
-  function onMouseMove(e) {
-    console.log("mousemove")
-
-    if (target.classList.contains('draggable')) {
-      target.style.position = "absolute";
-      target.style.left = e.pageX - shiftX + 'px';
-      target.style.top = e.pageY - shiftY + 'px';
-    }
-
-    /* if (targetRect.top < 0) {
-      window.scroll(0, 10)
-    } */
+  let rootElem = document.querySelector('html');
+  
+  let maximumRight = rootElem.clientWidth - target.offsetWidth;
+  console.log("rootElem.scrollTop: ", rootElem.scrollTop);
+  
+  let maximumBottom;
+  if (rootElem.scrollTop === 0) { // field 下面还有空间
+    maximumBottom = rootElem.clientHeight - target.offsetHeight;
+  } else {
+    maximumBottom = field.getBoundingClientRect().bottom + rootElem.scrollTop - target.offsetHeight;
   }
 
-  document.addEventListener('mousemove', onMouseMove);
+  if (target.classList.contains("draggable")) {
+    console.log("target: ", target);
+    let targetRect = target.getBoundingClientRect();
+    let shiftX = e.pageX - targetRect.left;
+    let shiftY = e.pageY - targetRect.top;
 
-  document.onmouseup = function(e) {
-    document.removeEventListener('mousemove', onMouseMove);
-    document.onmouseup = null;
+    function onMouseMove(e) {
+      console.log("mousemove");
+      let left = e.pageX - shiftX;
+      let top = e.pageY - shiftY;
+
+      if (top < 0) {
+        top = 0;
+      }
+
+      if (top > maximumBottom) {
+        top = maximumBottom - 1;
+      }
+
+      if (left < 0) {
+        left = 0;
+      }
+
+      if (left > maximumRight) {
+        left = maximumRight;
+      }
+
+      
+
+      target.style.position = "absolute";
+      target.style.left = left + "px";
+      target.style.top = top + "px";
+    }
+
+    document.addEventListener("mousemove", onMouseMove);
+
+    document.onmouseup = function (e) {
+      document.removeEventListener("mousemove", onMouseMove);
+      document.onmouseup = null;
+    };
   }
 };
 
